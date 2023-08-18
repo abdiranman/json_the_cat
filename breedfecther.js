af1aa8c2f533c8d8f5eedc46b3b38151e0092182
abdiranman <abdirahman.abdi330@.com>
2023-08-17 20:31:30 -0400

    Testing Cats as a Service

--- breedfecther.js
+++ breedfecther.js
@@ -1,36 +1,32 @@
 const request = require('request');
 
-const apiUrl = 'https://api.thecatapi.com/v1/invalid'; 
+const apiUrl = 'https://api.thecatapi.com/v1/breeds';
 
-// Get the breed name from command-line arguments
-const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log('Please provide a breed name as a command-line argument.');
-  process.exit(1);
-}
-const breedToSearch = args[0];
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Request error:', error); // Print the request error details
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('API request failed with status code:', response.statusCode);
-    return;
-  }
-
-  try {
-    const data = JSON.parse(body); // Parse the response body into an object
-    const breed = data.find(entry => entry.name.toLowerCase() === breedToSearch.toLowerCase());
-
-    if (breed) {
-      console.log('Description:', breed.description);
-    } else {
-      console.log(`Breed "${breedToSearch}" not found.`);
+const fetchBreedDescription = function(breedName, callback) {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
     }
-  } catch (parseError) {
-    console.error('Error parsing response:', parseError);
-  }
-});
+
+    if (response.statusCode !== 200) {
+      callback(`API request failed with status code: ${response.statusCode}`, null);
+      return;
+    }
+
+    try {
+      const data = JSON.parse(body);
+      const breed = data.find(entry => entry.name.toLowerCase() === breedName.toLowerCase());
+
+      if (breed) {
+        callback(null, breed.description);
+      } else {
+        callback(`Breed "${breedName}" not found.`, null);
+      }
+    } catch (parseError) {
+      callback(`Error parsing response: ${parseError}`, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
